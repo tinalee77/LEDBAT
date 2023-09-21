e166dfa526bf20999980cfe78ec09e36621d3934
tinalee77 <biz.tinalee@gmail.com>
2023-09-21 21:21:27 +0800

    updated tcp_ledbat_cong_avoid

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # LEDBAT
+
 Linux kernel module of LEDBAT (Conforming to RFC6817)
 
 How to use
-1. download the source code to a folder, e.g., ~/ledbat
-2. change the current directory to ~/ledbat
-3. issue the command: make
-4. then enter: sudo make install
-5. done!
+
+1. make
+2. sudo make install
--- tcp_ledbat.c
+++ tcp_ledbat.c
@@ -7,10 +7,7 @@
  *
  * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
  *
- * Created by Silvio Valenti on tue 2nd June 2009
- * 
- * Updated to conform to RFC6817 by Qian Li
- * 
+ * The source code of LEDBAT. It conforms to RFC6817. It is adapted from the code created by Silvio Valenti on tue 2nd June 2009. 
  */
 
 #include <linux/module.h>
@@ -34,12 +31,12 @@ static int min_cwnd = 2;
 module_param(min_cwnd, int, 0644);
 MODULE_PARM_DESC(min_cwnd, "min cwnd");
 
-/* NOTE: len are the array length - 1 */
+// NOTE: len are the array length - 1
 static int base_histo_len = 10;
 module_param(base_histo_len, int, 0644);
 MODULE_PARM_DESC(base_histo_len, "length of the base history vector");
 
-/* NOTE: len are the array length - 1 */
+// NOTE: len are the array length - 1 
 static int noise_filter_len = 1;
 module_param(noise_filter_len, int, 0644);
 MODULE_PARM_DESC(noise_filter_len, "length of the noise_filter vector");
@@ -55,7 +52,6 @@ MODULE_PARM_DESC(gain_inc, "increase gain");
 static int gain_dec = 1;
 module_param(gain_dec, int, 0644);
 MODULE_PARM_DESC(gain_dec, "decrease gain");
-
 
 /*
  * TCP-LEDBAT's state flags.
@@ -108,9 +104,6 @@ static void tcp_ledbat_release(struct sock *sk)
 	kfree(ledbat->base_history.buffer);
 }
 
-/**
- * tcp_ledbat_init
- */
 static void tcp_ledbat_init(struct sock *sk)
 {
 	struct ledbat *ledbat = inet_csk_ca(sk);
@@ -130,10 +123,6 @@ typedef s64 (*ledbat_filter_function) (struct owd_circ_buf *);
 
 static s64 ledbat_min_circ_buff(struct owd_circ_buf *b)
 {
-	/* 
-	   The draft requires all history to be set to +infinity on initialization. 
-	   We obtain the same behavior returning +infinity in case of empty history.
-	 */
 	if (b->first == b->next)
 		return (~0LLU) >> 1;
 	return b->buffer[b->min];
@@ -151,7 +140,6 @@ static s64 ledbat_base_delay(struct ledbat *ledbat)
 
 static u32 tcp_ledbat_ssthresh(struct sock *sk)
 {
-	struct inet_sock *inet = inet_sk(sk);
 	u32 res;
 	
 	res = tcp_reno_ssthresh(sk);
@@ -161,12 +149,18 @@ static u32 tcp_ledbat_ssthresh(struct sock *sk)
 
 static void tcp_ledbat_cwnd_event(struct sock *sk, enum tcp_ca_event ev)
 {
-	struct inet_sock *inet = inet_sk(sk);
 	struct ledbat *ledbat = inet_csk_ca(sk);
+	struct tcp_sock *tp = tcp_sk(sk);
+	s32 idle;
 
 	switch (ev) {
-	case CA_EVENT_CWND_RESTART: // after idle, cwnd is restarted
-		ledbat->flag |= LEDBAT_CAN_SS;
+	case CA_EVENT_CWND_RESTART: // after idle
+		if (READ_ONCE(sock_net(sk)->ipv4.sysctl_tcp_slow_start_after_idle)) {
+			idle = tcp_jiffies32 - tp->lsndtime;
+			if (idle > inet_csk(sk)->icsk_rto) {
+				ledbat->flag |= LEDBAT_CAN_SS;
+			}
+		}
 		ledbat->byte_cnt = 0;
 		break;
 	case CA_EVENT_COMPLETE_CWR: // after fast retransmit and fast recovery
@@ -193,23 +187,17 @@ static bool is_cwnd_limited(struct sock *sk)
 	return tp->is_cwnd_limited;
 }
 
-/**
- * tcp_ledbat_cong_avoid
- */
 static void tcp_ledbat_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 {
 	struct ledbat *ledbat = inet_csk_ca(sk);
 	struct tcp_sock *tp = tcp_sk(sk);
-	struct inet_sock *inet = inet_sk(sk);
 	s64 current_delay, base_delay, queue_delay, off_target, gain, cwnd, adj_b, adj_p;
-	u64 max_allowed_cwnd = 0;
 	
-	/*if no valid data return */
 	if (!(ledbat->flag & LEDBAT_VALID_RHZ)) {
 		return;
 	}
 
-	if (do_ss && tp->snd_cwnd < tp->snd_ssthresh && (ledbat->flag & LEDBAT_CAN_SS)) {
+	if (do_ss && (ledbat->flag & LEDBAT_CAN_SS) && tp->snd_cwnd < tp->snd_ssthresh) {
 		acked = tcp_slow_start(tp, acked);
 		if (!acked)
 			return;
@@ -221,7 +209,6 @@ static void tcp_ledbat_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 	off_target = target - queue_delay;
 	if (off_target >= 0) {
 		if (!is_cwnd_limited(sk)) {
-			//ledbat->byte_cnt = 0;
 			return;
 		}
 		gain = gain_inc;
@@ -234,20 +221,16 @@ static void tcp_ledbat_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 	if (tp->mss_cache)
 		adj_p = div_s64(adj_b, tp->mss_cache);
 	if (adj_p) {
-		ledbat->byte_cnt -= adj_b * tp->snd_cwnd * target;
-		max_allowed_cwnd = min_t(u64, (u64)tp->snd_cwnd + 1, tp->snd_cwnd_clamp);
-		cwnd = tp->snd_cwnd + adj_p;
-		cwnd = min_t(s64, cwnd, max_allowed_cwnd);
+		adj_b -= adj_p * tp->mss_cache;
+		ledbat->byte_cnt = adj_b * tp->snd_cwnd * target;
+		cwnd = min_t(s64, tp->snd_cwnd_clamp, tp->snd_cwnd + adj_p);
 		cwnd = max_t(s64, cwnd, min_cwnd);
 		tp->snd_cwnd = cwnd;
 	}
 }
 
-/**
- * tcp_ledbat_remote_hz_estimator
- *
- * Estimate remote HZ.
- * We keep on updating the estimated value, where original TCP-LP
+/*
+ * We keep on updating the estimated value, while the original TCP-LP
  * implementation only guesses it once and uses it forever.
  */
 static void tcp_ledbat_remote_hz_estimator(struct sock *sk)
@@ -262,53 +245,42 @@ static void tcp_ledbat_remote_hz_estimator(struct sock *sk)
 		goto out;
 	}
 	
-	if (env && hz) { // internet but remote hz is known
+	if (env && hz) { // environment is Internet but remote hz is known
 		rhz = hz << 6;
 		goto out;
 	}
 	
-	// working environment is internet
-
-	/* not yet record reference time
-	 * go away!! record it before come back!! */
 	if (ledbat->remote_ref_time == 0 || ledbat->local_ref_time == 0)
 		goto out;
 
-	/* we can't calc remote HZ with no difference!! */
 	if (tp->rx_opt.rcv_tsval <= ledbat->remote_ref_time || tp->rx_opt.rcv_tsecr <= ledbat->local_ref_time)
 		goto out;
 
-	// m > 0
 	m = TCP_TS_HZ * (tp->rx_opt.rcv_tsval - ledbat->remote_ref_time) / (tp->rx_opt.rcv_tsecr - ledbat->local_ref_time);
 
 	if (rhz) {
-		m -= rhz >> 6;	/* m is now error in remote HZ est */
-		rhz += m;	/* 63/64 old + 1/64 new */
-	} else
+		m -= rhz >> 6;	// m is now estimation error 
+		rhz += m;	// 63/64 old + 1/64 new 
+	} else {
 		rhz = m << 6;
+	}
 
 	out:
 	ledbat->remote_hz = rhz >> 6;
 
-	/* record time for successful remote HZ calc, rhz > 0 for sure */
 	if (ledbat->remote_hz)
 		ledbat->flag |= LEDBAT_VALID_RHZ;
 	else
 		ledbat->flag &= ~LEDBAT_VALID_RHZ;
 
-	/* record reference time stamp */
 	ledbat->remote_ref_time = tp->rx_opt.rcv_tsval;
 	ledbat->local_ref_time = tp->rx_opt.rcv_tsecr;
 }
 
-/**
- * tcp_ledbat_owd_calculator
- */
 static s64 tcp_ledbat_owd_calculator(struct sock *sk)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct ledbat *ledbat = inet_csk_ca(sk);
-	struct inet_sock *inet = inet_sk(sk);
 	s64 owd = 0;
 	
 	tcp_ledbat_remote_hz_estimator(sk);
@@ -325,26 +297,20 @@ static void ledbat_add_delay(struct owd_circ_buf *cb, s64 owd)
 	u8 i;
 
 	if (cb->next == cb->first) {
-		/*buffer is empty */
 		cb->buffer[cb->next] = owd;
 		cb->min = cb->next;
 		cb->next++;
 		return;
 	}
 
-	/*set the new delay */
 	cb->buffer[cb->next] = owd;
-	/* update the min if it is the case */
 	if (owd < cb->buffer[cb->min])
 		cb->min = cb->next;
 
-	/* increment the next pointer */
 	cb->next = (cb->next + 1) % cb->len;
 
 	if (cb->next == cb->first) {
-		/* Discard the first element */
 		if (cb->min == cb->first) {
-			/* Discard the min, search a new one */
 			cb->min = i = (cb->first + 1) % cb->len;
 			while (i != cb->next) {
 				if (cb->buffer[i] < cb->buffer[cb->min])
@@ -352,7 +318,6 @@ static void ledbat_add_delay(struct owd_circ_buf *cb, s64 owd)
 				i = (i + 1) % cb->len;
 			}
 		}
-		/* move the first */
 		cb->first = (cb->first + 1) % cb->len;
 	}
 }
@@ -372,17 +337,14 @@ static void ledbat_update_base_delay(struct ledbat *ledbat, s64 owd)
 		ledbat->last_rollover = tcp_jiffies32;
 
 	if (ledbat->base_history.next == ledbat->base_history.first) {
-		/* empty circular buffer */
 		ledbat_add_delay(cb, owd);
 		return;
 	}
 
 	if (tcp_jiffies32 - ledbat->last_rollover > msecs_to_jiffies(60 * MSEC_PER_SEC)) {
-		/* we have finished a minute */
 		ledbat->last_rollover = tcp_jiffies32;
 		ledbat_add_delay(cb, owd);
 	} else {
-		/* update the last value and the min if it is the case */
 		last = (cb->next + cb->len - 1) % cb->len;
 		if (owd < cb->buffer[last]) {
 			cb->buffer[last] = owd;
@@ -392,19 +354,11 @@ static void ledbat_update_base_delay(struct ledbat *ledbat, s64 owd)
 	}
 }
 
-/**
- * tcp_ledbat_rtt_sample
- *
- * - calculate the owd
- * - add the delay to noise filter
- * - if new minute add the delay to base delay or update last delay 
- */
 static void tcp_ledbat_rtt_sample(struct sock *sk, u32 rtt)
 {
 	struct ledbat *ledbat = inet_csk_ca(sk);
 	s64 mowd = tcp_ledbat_owd_calculator(sk);
 
-	/* sorry that we don't have valid data */
 	if (!(ledbat->flag & LEDBAT_VALID_RHZ)) {
 		return;
 	}
@@ -413,13 +367,8 @@ static void tcp_ledbat_rtt_sample(struct sock *sk, u32 rtt)
 	ledbat_update_base_delay(ledbat, mowd);
 }
 
-/**
- * tcp_ledbat_pkts_acked
- */
 static void tcp_ledbat_pkts_acked(struct sock *sk, const struct ack_sample *sample)
 {
-	struct inet_sock *inet = inet_sk(sk);
-	
 	if (sk->sk_state == TCP_SYN_SENT || sk->sk_state == TCP_SYN_RECV || sk->sk_state == TCP_NEW_SYN_RECV || sk->sk_state == TCP_LISTEN) {
 		return;
 	}
@@ -428,15 +377,9 @@ static void tcp_ledbat_pkts_acked(struct sock *sk, const struct ack_sample *samp
 		tcp_ledbat_rtt_sample(sk, sample->rtt_us);
 }
 
-/**
- * tcp_ledbat_undo_cwnd
- * required starting kernel v4.10, see
- * kernel commmit e97991832a4ea4a5f47d65f068a4c966a2eb5730
- */
 static u32 tcp_ledbat_undo_cwnd(struct sock *sk)
 {
 	const struct tcp_sock *tp = tcp_sk(sk);
-	struct inet_sock *inet = inet_sk(sk);
 	
 	return max(tp->snd_cwnd, tp->snd_ssthresh << 1);
 }
@@ -469,6 +412,6 @@ static void __exit tcp_ledbat_unregister(void)
 module_init(tcp_ledbat_register);
 module_exit(tcp_ledbat_unregister);
 
-MODULE_AUTHOR("Silvio Valenti and Qian Li");
+MODULE_AUTHOR("Qian Li");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("TCP Ledbat (Low Extra Delay Background Transport)");
+MODULE_DESCRIPTION("TCP LEDBAT");
